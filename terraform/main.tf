f4bf106ddada0bcb27ccb5f5f02125eec760559b
Hitish Rao P <hitishraop@gmail.com>
2025-04-07 19:11:45 +0530

    [STAGE] migrating to tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,48 @@
 provider "aws" {
   region                      = var.aws_region
+  secret_key                  = var.aws_secret_key
+  access_key                  = var.aws_access_key
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 
   endpoints {
     ecr = var.localstack_endpoint
   }
+}
+
+resource "aws_ecs_cluster" "rolt" {
+  name = "rolt"
+}
+
+resource "aws_ecr_repository" "rolt" {
+  name = "rolt"
+}
+
+resource "aws_dynamodb_table" "build_logs" {
+  name = "build_logs"
+}
+
+resource "aws_dynamodb_table" "production_logs" {
+  name = "production_logs"
+}
+
+resource "aws_vpc" "rolt_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "rolt-vpc"
+    resource : "vpc"
+  }
+}
+
+resource "aws_subnet" "rolt_public_subnet" {
+  vpc_id     = aws_vpc.rolt_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    resource = "subnet"
+    name     = "rolt_public_subnet"
+  }
+}
+
+resource "aws_sqs_queue" "deployer_queue" {
+  name = "deployer_queue"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "localstack_endpoint" {
   type = string
   default = "http://localstack:4566"
 }
+
+variable "aws_secret_key" {
+  type = string
+  default = "test"
+}
+
+variable "aws_access_key" {
+  type = string
+  default = "test"
+}
